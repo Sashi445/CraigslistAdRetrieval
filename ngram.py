ed6b9d21ab03d2c9d608a2f651e6c22650958792
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-11-28 18:20:11 +0530

    added word-count

--- ngram.py
+++ ngram.py
@@ -4,40 +4,53 @@ import json
 
 os.chdir('corpus')
 
-file = open('corpus.txt','r',encoding='utf-8')
-sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',file.read())
-file.close()
-
-for i in range(0,len(sentences)):
-    sentence = sentences[i].lower()
-    sentence = re.sub(r'\W',' ',sentence)        # removing non-word symbols
-    sentence = re.sub(r'\s+',' ',sentence)       # replacing extra spaces with single space
-    sentence = sentence.strip()
-    sentences[i] = sentence
+index = {}
+words = {}
 
 n = int(input('Enter the value of n : '))
 
-print(sentences)
+for file in os.listdir():
+    f = open(file,'r',encoding='utf-8')  
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',f.read())
+    f.close()
 
-index = {}
-for sentence in sentences:
-    if n > len(sentence):
-        if sentence in index.keys():
-            index[sentence]+=1
-        else:
-            index[sentence]=1
-    else:
-        for j in range(0,len(sentence)-n):
-            gram = ''.join(sentence[j:j+n])
-            if gram in index.keys():
-                index[gram]+=1
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = re.sub(r'\W',' ',sentence)        # removing non-word symbols
+        sentence = re.sub(r'\s+',' ',sentence)       # replacing extra spaces with single space
+        sentence = sentence.strip()
+
+        # n-gram index
+        if n > len(sentence):
+            if sentence in index.keys():
+                index[sentence]+=1
             else:
-                index[gram]=1 
+                index[sentence]=1
+        else:
+            for j in range(0,len(sentence)-n):
+                gram = ''.join(sentence[j:j+n])
+                if gram in index.keys():
+                    index[gram]+=1
+                else:
+                    index[gram]=1 
 
+        # word-count index
+        for word in sentence.split(' '):
+            if word!=' ' and word!='':
+                if word not in words.keys():
+                    words[word]=1
+                else:
+                    words[word]+=1
+    
 os.chdir('..')
 
-with open(str(n)+'_gram.json','w',encoding='utf-8') as file:
+with open(str(n)+'-gram.json','w',encoding='utf-8') as file:
     json.dump(index,file,indent=4)
-    file.close()    
+    file.close()   
 
+with open('word-count.json','w',encoding='utf-8') as file:
+    json.dump(words,file,indent=4)
+    file.close()
+
+print('no.of words in the corpus {}'.format(len(words)))
 print('No of grams in the corpus {}'.format(len(index)))
