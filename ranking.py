e0b5da3349477f60d03713c729998cb452f9308b
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-12-17 21:34:48 +0530

    added retrieval

--- ranking.py
+++ ranking.py
@@ -34,3 +34,8 @@ values = zip(coefficients.keys(), coefficients.values())
 results = sorted(values,  key=lambda x : x[1], reverse=True)
 
 print(results)
+
+retrieval = [results[i][0] for i in range(10)]
+with open('retrieval.json','w',encoding='utf-8') as f:
+    json.dump(retrieval,f,indent=4)
+    f.close()
