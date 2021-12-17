5105529203d2419abb569b828680f0d0e90a0d39
manoj-b-22 <manojsrinivas.b19@iiits.in>
2021-12-17 21:39:11 +0530

    fixed

--- ranking.py
+++ ranking.py
@@ -1,15 +1,6 @@
 import json
 from tf_idf import DocumentRanker
 
-
-# n = int(input('Enter the value of n : '))
-
-# f = open(str(n)+'-gram.json','r')
-# ngram = json.load(f)
-# f.close()
-
-# print('Use can use * as wildcard query')
-# print('Caution use only one * per word 2 or more are not allowed')
 query = input('Enter the query:').split(' ')
 
 
@@ -21,13 +12,9 @@ d.set_word_count()
 
 d.get_idf_values()
 
-# d.create_document_vector_matrix()
-
 d.get_doc_vectors()
 
 coefficients = d.find_similarity_coefficients(query=query)
-
-# print(coefficients)
 
 values = zip(coefficients.keys(), coefficients.values())
 
